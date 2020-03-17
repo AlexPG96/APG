c90f138054c63fff35d2d328c2c086b2675b8f97
AlexPG96 <ap96.gonzalez@gmail.com>
2020-03-17 16:12:25 +0100

    my app con botones y fotos

--- src/App.js
+++ src/App.js
@@ -28,21 +28,22 @@ class Lista extends React.Component {
     this.state = {
       colection: [
         {
-          image: '',
+          image: 'moto.jpg',
           title: 'Te vendo una moto',
-          text: 'descripcion',
+          description: 'moto',
+          text: 'description',
           value: '1.000€'
         },
         {
-          image: '',
+          image: 'coche.jpg',
           title: 'Te vendo un coche',
-          text: 'descripcion',
+          text: 'description',
           value: '1.800€'
         },
         {
-          image: '',
+          image: 'furgoneta.jpg',
           title: 'Te vendo una furgoneta',
-          text: 'descripcion',
+          text: 'description',
           value: '3.500€'
         }
 
@@ -55,7 +56,10 @@ class Lista extends React.Component {
     return(
       <ul>
         {colection.map((uno, dos) => {
-          return <li key={dos}>{uno.image}{uno.title}{uno.text}{uno.value}</li>
+          return <li key={dos}><img src={uno.image} alt ={uno.description}/>{uno.title}{uno.text}<button>-</button><button>+</button>{uno.value}
+          </li>
+
+          <input type="button">-</input>
         })
         }
       </ul>
@@ -91,70 +95,4 @@ class App extends React.Component {
 export default App;
 */
 
-/*
-import React from "react";
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      texto: 'Yo tambien tengo gatos',
-    }
-    // this.actualizar = this.actualizar.bind(this)
-  }
-  // actualizar( evento ) {
-  actualizar = (evento) => {
-    console.log(evento.target.value)
-    this.setState({texto: evento.target.value})
-  }
-  render(){
-    const {texto} = this.state;
-    return (
-      <>
-        <Titulo txt={texto}/>
-        <Input cambiaElEstado={this.actualizar}/>
-        <Lista />
-      </>
-    )
-  }
-}
-class Lista extends React.Component {
-  constructor() {
-    super();
-      this.state = {
-        coleccion: [
-          {nombre: 'El nombre de la Rosa'},
-          {nombre: 'Pinocho'},
-          {nombre: 'Kamasutra'},
-          {nombre: 'El cantar de los cantares'},
-        ]
-      }
-  }
-  render() {
-    const {coleccion} = this.state;
-    return (
-      <ul>
-        {coleccion.map((libro, index) => {
-          return <li key={index}>{libro.nombre}</li>
-        })}
-      </ul>
-    )
-  }
-}
-class Input extends React.Component {
-  render() {
-    const {cambiaElEstado} = this.props;
-    return (
-      <input type="text" onChange={cambiaElEstado} />
-    )
-  }
-}
-class Titulo extends React.Component {
-  render() {
-    const {txt} = this.props
-    return (
-      <h1>{txt}</h1>
-    )
-  }
-}
-*/
 
